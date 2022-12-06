bb3bab0128a6c0ecec03e3667b89629fc7851afe
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-12-06 08:41:28 -0500

    started working with login screen

--- app.py
+++ app.py
@@ -21,6 +21,13 @@ def index() -> str:
     )
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template(
+        "login.html",
+    )
+
+
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
