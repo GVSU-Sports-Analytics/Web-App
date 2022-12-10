1cdd8b0f9ca7642834168ad030fdd60895836881
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-12-10 00:16:35 -0500

    database configured with environment vars and clean flask_app code that will work on local and live prod

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,12 @@
 from flask import Flask
-from blueprints.about import about
-from blueprints.baseball import bsbl
+from blueprints.baseball import baseball
 from blueprints.softball import softball
 from blueprints.football import football
 from blueprints.basketball import basketball
 from blueprints.index import index
-
-# import os
-# import mysql.connector
+from flask_sqlalchemy import SQLAlchemy
+from socket import gethostname
+import os
 
 app = Flask(
     __name__,
@@ -15,20 +14,34 @@ app = Flask(
     static_folder="static"
 )
 
+username = os.getenv("DB_USERNAME")
+password = os.getenv("DB_PASSWORD")
+hostname = os.getenv("DB_HOST")
+databasename = os.getenv("DB_NAME")
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username=username,
+    password=password,
+    hostname=hostname,
+    databasename=databasename,
+)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 
 def register_blueprints() -> None:
     app.register_blueprint(index)
-    app.register_blueprint(about)
-    app.register_blueprint(bsbl)
+    app.register_blueprint(baseball)
     app.register_blueprint(football)
     app.register_blueprint(basketball)
     app.register_blueprint(softball)
 
 
-# need to configure mysql database connection properly still
-
 if __name__ == "__main__":
     register_blueprints()
-    app.run(
-        debug=True,
-    )
+    db = SQLAlchemy(app)
+
+    if 'liveconsole' not in gethostname():
+        app.run(debug=True)
