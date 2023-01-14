6a1b2da50a987e048a64f3c6803572eac4b58afe
Jensen Holm <holmj@mail.gvsu.edu>
2023-01-14 17:04:29 -0500

    got the dockerfile working

--- README.md
+++ README.md
@@ -4,7 +4,9 @@
 ### Run Locally Without Dockerfile
 `git clone https://github.com/GVSU-Sports-Analytics/Web-App` <br>
 `pip3 install -r requirements.txt` <br>
-`gunicorn --bind :3000 --workers 1 --threads 8 --timeout 0 app:app`
+`gunicorn --bind :3000 --workers 1 --threads 8 --timeout 0 app:app`<br>
 
-### Run Locally With Docker
-``
+### Setting up Dockerfile
+`sudo docker build -t gvapp .` <br>
+`sudo docker run gvapp` <br>
+
--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask, request, redirect
+from flask import Flask
+
 from blueprints.baseball import baseball
 from blueprints.softball import softball
 from blueprints.football import football
 from blueprints.basketball import basketball
 from blueprints.index import index
 from register import register_blueprints
-
-import json
-import requests
 
 app = Flask(
     __name__,
@@ -24,12 +22,9 @@ register_blueprints(
     app=app,
 )
 
-
-
-
 if __name__ == "__main__":
     #  gunicorn --bind :3000 --workers 1 --threads 8 --timeout 0 app:app
     app.run(
         debug=True,
-        host="0.0.0.0"
+        port=8080,
     )
