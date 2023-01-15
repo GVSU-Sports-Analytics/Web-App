24dc6d1e2d4377bb3d0600c1bd2fd3da67916a1e
Jensen Holm <holmj@mail.gvsu.edu>
2023-01-15 14:59:54 -0500

    got it setup to where upon requesting the baseball page, the api is called, data collected, and pictures and some info about the players is shown on the page

--- blueprints/baseball.py
+++ blueprints/baseball.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 
 # for development only
+# this update logic will be moved to the sidearm updater repo
 from blueprints.index import update
 
 baseball = Blueprint(
@@ -14,16 +15,13 @@ baseball = Blueprint(
 @baseball.route("/baseball")
 def _baseball() -> str:
     # get the baseball data from database
-
     # for now, we are calling the api whenever
     # the page refreshes so that we can at lease see the data
     # until we get a database
     data = update()
-    _2021 = data["_2021"]
-    print(_2021)
     return render_template(
         "sport.html",
         in_progress=True,
         sport="GVSU Baseball",
-        data=_2021,
+        data=data["_2021"],
     )
