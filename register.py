e5ab01bab7760193e6c9f73361a54c37a7c4cf4f
Jensen Holm <holmj@mail.gvsu.edu>
2023-01-15 22:47:37 -0500

    caught up the docker file

--- register.py
+++ register.py
@@ -1,21 +1,6 @@
 from flask import Flask
 import os
 
-import pymysql
-
-
-def config_db():
-    user_name = os.environ["GVAPP_USERNAME"]
-    password = os.environ["GVAPP_PASSWORD"]
-    host = os.environ["GVAPP_HOST"]
-
-    db = pymysql.connect(
-        host=host,
-        user=user_name,
-        password=password
-    )
-    return db
-
 
 def register_blueprints(*args, app: Flask) -> None:
     for bp in args:
