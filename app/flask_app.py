9862f62075acd4b53a487bf390806930455c5eba
Jensen Holm <holmj@mail.gvsu.edu>
2023-01-16 13:00:12 -0500

    made the app class to hopefully have cleaner code when accessing the database

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,26 +1,58 @@
 from dataclasses import dataclass, field
 from flask import Flask, Blueprint
+import sqlite3
 
 
 @dataclass
 class App:
     name: str
     port: int
+    template_path: str
+    static_path: str
 
-    _self: Flask = field(
-            default_factory=lambda: Flask(__name__)
-        )
+    _db: sqlite3
+    _cursor: sqlite3.Cursor
+
+    _blueprints: list[Blueprint] = field(
+        default_factory=lambda: []
+    )
+    _app: Flask = field(
+        default_factory=lambda: Flask(__name__)
+    )
 
     @property
-    def name(self) -> str:
+    def Name(self) -> str:
         return self.name
 
     @property
-    def port(self) -> int:
+    def Port(self) -> int:
         return self.port
 
-    def config(blueprints: list[Blueprint]) -> None:
-        return
+    @property
+    def Database(self):
+        return self._db
+
+    @property
+    def Cursor(self):
+        return self._cursor
+
+    @property
+    def App(self):
+        return self._app
+
+    def add_blueprints(self, *blueprints):
+        for bp in blueprints:
+            self._blueprints.append(bp)
+
+    def config(self):
+        for bp in self._blueprints:
+            self._app.register_blueprint(bp)
 
     def run(self):
-        return
+        self.config()
+        self._app.run(
+            debug=True,
+            port=self.port
+        )
+        self.Database.close()
+        self.Cursor.close()
