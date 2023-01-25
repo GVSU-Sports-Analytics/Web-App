675ffca13104d158565aa43413875c16cb3d2cc4
Jensen-holm <holmj@mail.gvsu.edu>
2023-01-24 23:56:52 -0500

    getting started with custom components and assets

---
+++ src/components/Louie.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+
+export default function HeaderImage() {
+    return (
+        <Image 
+        src="/../public/louie.gif"
+        alt="louie-bg"
+        width={700}
+        height={700}
+        >
+        </Image>
+    )
+}
---
+++ src/components/Nav.tsx
@@ -0,0 +1,10 @@
+import { Navbar, Container } from "@mantine/core"
+
+
+export default function Nav() {
+    return (
+        <Navbar>
+        </Navbar>
+    )
+
+}
